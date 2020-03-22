21969d37703fc81d439541efb0dcae1682ece646
Oliver Cass <olicass99@gmail.com>
2020-03-22 17:51:52 +0000

    more sorting...

--- chinese_paintings/node.js.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for %%* in (.) do set projectName=%%~nx*
-set loc=%CD%
-cd /d C:\Program Files\nodejs
-start nodevars.bat
--- chinese_paintings/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Chinese Painting Server
-@echo on
-node index.js
-pause>nul
-exit /b
